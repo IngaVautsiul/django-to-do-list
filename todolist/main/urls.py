846447f97e4d6fc4c7f7db5c835ab5b5c862a955
nga <inga.vaitsiul@gmail.com>
2022-01-05 20:38:20 +0300

    create tasks and add cteate_task method

--- todolist/main/urls.py
+++ todolist/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('about-us', views.about, name="about")
+    path('about-us', views.about, name="about"),
+    path('create_task', views.create_task, name="create_task")
 ]
